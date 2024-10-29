5ef6e63317bedd004cce82e5e4bb80c3046b686f
Alexandra Kolpakova <alexandra.k.w@yandex.ru>
2024-10-30 00:11:32 +0600

    feat(popup): add popup with 10s timer via observable

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private popupObservable: Observable<HTMLElement | null>;
+  private popupSubscription: Subscription | null = null;
+  private popup: HTMLElement | null = null;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() {
+    this.popupObservable = new Observable<HTMLElement | null>(observer => {
+      const timeout = setTimeout(() => {
+        observer.next();
+      }, 10000)
+      return {
+        unsubscribe() {
+          clearTimeout(timeout);
+        }
+      }
+    })
   }
 
+  ngOnInit(): void {
+    this.popup = document.getElementById('popupModal');
+    this.popupSubscription = this.popupObservable.subscribe({
+      next: () => {
+        console.log('hi');
+        if (this.popup) {
+          document.getElementById('popupButton')?.click();
+        }
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.popupSubscription?.unsubscribe();
+  }
 }
