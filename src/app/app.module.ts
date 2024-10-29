a63c4ebdcea9441f6916a09d68b8acb9933108ed
Alexandra Kolpakova <alexandra.k.w@yandex.ru>
2024-10-29 21:44:55 +0600

    refactor: create components for each future page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { FooterComponent } from './common/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { ProductComponent } from './pages/product/product.component';
+import { OrderComponent } from './pages/order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    CatalogComponent,
+    ProductComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
