11454dd95fb9490610d73ba11ad3822b7f7a411f
Alexandra Kolpakova <alexandra.k.w@yandex.ru>
2024-10-31 20:58:21 +0600

    feat: add order-form validation

--- src/app/pages/order/order.component.ts
+++ src/app/pages/order/order.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, Validators} from "@angular/forms";
 import {ActivatedRoute} from "@angular/router";
+import {Observable, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-order',
@@ -10,30 +11,40 @@ import {ActivatedRoute} from "@angular/router";
 export class OrderComponent implements OnInit {
 
   orderForm = this.fb.group({
-    product: ['', Validators.required],
+    product: [''],
     personalInfo: this.fb.group({
-      name: ['', Validators.required],
-      last_name: ['', Validators.required],
-      phone: ['', Validators.required],
+      name: ['', [Validators.required, Validators.pattern('^[a-zA-Zа-яА-Я]+$')]],
+      last_name: ['', [Validators.required, Validators.pattern('^[a-zA-Zа-яА-Я]+$')]],
+      phone: ['', [Validators.required, Validators.pattern('^\\+?[0-9]{11}$')]],
     }),
     addressInfo: this.fb.group({
       country: ['', Validators.required],
       zip: ['', Validators.required],
-      address: ['', Validators.required],
+      address: ['', [Validators.required, Validators.pattern('^[a-zA-Zа-яА-Я\\d\\s\\-/]+$')]],
     }),
-    comment: ['', Validators.required],
+    comment: [''],
   })
 
   constructor(private fb: FormBuilder,
-              private activatedRoute: ActivatedRoute,) {
+              private activatedRoute: ActivatedRoute,) {}
 
-  }
+  get personalName() {return this.orderForm.get('personalInfo')?.get('name')};
+  get personalLastName() {return this.orderForm.get('personalInfo')?.get('last_name')};
+  get personalPhone() {return this.orderForm.get('personalInfo')?.get('phone')};
+  get addressInfo() {return this.orderForm.get('addressInfo')?.get('address')};
+  get addressCountry() {return this.orderForm.get('addressInfo')?.get('country')};
+  get addressZip() {return this.orderForm.get('addressInfo')?.get('zip')};
 
   ngOnInit(): void {
     this.activatedRoute.queryParams.subscribe((params) => {
       if(params['product']) {
         this.orderForm.patchValue({product: params['product']})
+        this.orderForm.controls['product'].disable();
       }
     })
+  }
+
+  createOrder() {
+
   }
 }
